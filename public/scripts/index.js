e04e08882d2ac6682c14dd95acb1c18eaae0b637
Ofek Atar <ofek.atr@gmail.com>
2020-12-10 20:01:51 +0200

    Implemented Animation.

---
+++ public/scripts/index.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusHandler() {
+    const parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurHandler() {
+    const parent = this.parentNode.parentNode;
+    parent.classList.remove("focus");
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusHandler);
+    input.addEventListener("blur", blurHandler);
+})
